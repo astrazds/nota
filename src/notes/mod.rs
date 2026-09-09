2313514d65d5aea8924c4244b749e160c28bd5a0
Andrejs Strazds <andrejs@astrazds.com>
2026-09-09 16:25:40 +1000

    refactor(core): own shared modules inside nota-core

--- src/notes/mod.rs
+++
@@ -1,8 +0,0 @@
-pub(crate) mod collection;
-pub(crate) mod discovery;
-pub(crate) mod list_interaction;
-pub(crate) mod model;
-pub(crate) mod sample;
-pub(crate) mod search_query;
-pub(crate) mod tag_rules;
-pub(crate) mod workspace;
